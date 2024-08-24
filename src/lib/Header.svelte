309b5ee0915733bf9a57ac1f776398eadef12376
gginglis <inglisge23@craighead.school.nz>
2024-08-24 14:59:48 +1200

    each item can only be added once

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -11,12 +11,14 @@
     font-size: 3rem;
     font-weight: 500;
     color: rgba(255, 255, 255, 0.9);
+    text-shadow: 1px 1px rgb(82, 118, 82);
   }
   h2 {
     font-family: "Urbanist", sans-serif;
     font-weight: 600;
     font-size: 1.75rem;
     color: rgba(255, 255, 255, 0.589);
+    text-shadow: 1px 1px rgb(82, 118, 82);
   }
   header {
     background-color: rgb(147, 175, 147);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from "$lib/Header.svelte";
   import Item from "$lib/Item.svelte";
+  import { onMount } from "svelte";
 
   async function getMenu() {
     let menuData = await fetch(
@@ -12,8 +13,26 @@
 
   let custom = [];
 
-  function addToMenu(item) {
-    custom = [...custom, item];
+  function selectedItem() {
+    selected = !selected;
+  }
+
+  function addToMenu(food) {
+    custom = [...custom, food];
+    calculateGst;
+  }
+
+  function removeFromMenu(index) {
+    custom = [...custom.slice(0, index), ...custom.slice(index + 1)];
+  }
+
+  let gst = 0.15;
+
+  function calculateGst() {
+    gst = 0;
+    for (let gst of food) {
+      gst = food.price * gst;
+    }
   }
 </script>
 
@@ -33,21 +52,46 @@
         loading
       {:then menu}
         {#each menu.breakfast as breakfast}
-          <Item {...breakfast} />
-          <button on:click={() => addToMenu()}>add to menu</button>
+          <div class:highlight={breakfast.selected}>
+            <Item {...breakfast} />
+            <button
+              on:click={() => addToMenu(breakfast)}
+              disabled={custom.includes(breakfast)}>add to menu</button
+            >
+          </div>
         {/each}
+
         {#each menu.dinner as dinner}
           <Item {...dinner} />
-          <button on:click={() => addToMenu()}>add to menu</button>
+          <button
+            on:click={() => addToMenu(dinner)}
+            disabled={custom.includes(dinner)}>add to menu</button
+          >
         {/each}
         {#each menu.dessert as dessert}
           <Item {...dessert} />
-          <button on:click={() => addToMenu()}>add to menu</button>
+          <button
+            on:click={() => addToMenu(dessert)}
+            disabled={custom.includes(dessert)}>add to menu</button
+          >
         {/each}
       {/await}
     </div>
     <div class="column">
-      <h2><input type="text" /></h2>
+      <h2>custom menu</h2>
+      {#if custom.length >= 1}
+        {#each custom as chosen, index}
+          <p>${gst}</p>
+          <Item {...chosen} />
+          <button
+            on:click={() => {
+              removeFromMenu(index);
+            }}>remove</button
+          >
+        {/each}
+      {:else}
+        <p>menu has no items</p>
+      {/if}
     </div>
   </div>
 </main>
@@ -57,4 +101,21 @@
 </footer>
 
 <style>
+  h2 {
+    font-family: "Anton SC", sans-serif;
+    font-size: 2.4rem;
+    font-weight: 500;
+    margin: 30px;
+    color: rgb(147, 175, 147);
+    text-shadow: 0.7px 0.7px rgb(82, 118, 82);
+  }
+  .highlight {
+    background-color: rgb(147, 175, 147);
+  }
+
+  p {
+    font-family: "Urbanist", sans-serif;
+    font-weight: 400;
+    font-size: 1.1rem;
+  }
 </style>
